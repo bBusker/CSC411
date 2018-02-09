b8116059ed696a584a791bf625560355f7cad783
Alex Wang <alexwang@Alexs-MacBook-Pro.local>
2018-02-09 16:33:56 -0500

    Part 3 done except for vector checking

--- Project2/digits.py
+++ Project2/digits.py
@@ -17,13 +17,13 @@ from scipy.io import loadmat
 #Load the MNIST digit data
 M = loadmat("mnist_all.mat")
 
-'''PART 1'''
+#PART 3
 
 #save 10 images, 1 from each digit
 # for i in range(10):
 #     imsave("part1/image" + str(i) + ".png", M["train"+str(i)][10].reshape((28,28)))
 
-'''PART 2'''
+#PART 2
 
 def compute_2(x, W, b)
     return softmax(tanh_layer(x, W, b))
@@ -40,10 +40,24 @@ def tanh_layer(y, W, b):
     is the number of cases'''
     return tanh(dot(W.T, y)+b)
 
-'''PART 3'''
-#looking to write the cost function
-
+#PART 3
 def f(x, W, b, y):
     res = compute_2(x,W,b)
     return -1 * (dot(y.T, np.log(res)))
+
+#PART 3a)
+'''We look to write our gradient function with respect to each weight used in our neural network W_ij. In the slides we've 
+provided an expression for the cost of the cost function with respect to the output pre-softmax. To obtain the change in 
+cost w.r.t our weightings, we employ calculus getting dC/dW_ij = dC/do_i * do_i / dW_ij.
+
+do_i / dW-ij is x_i
+ '''
+
+#PART 3b)
+def df(x,y,t):
+    return np.matmul((y-t).T, x)
+
+#verification code
+
+
 
