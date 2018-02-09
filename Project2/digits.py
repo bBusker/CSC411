01315bbd44a9ea37d5c9b4004ddb54c2548928f4
Alex Wang <alexwang@Alexs-MacBook-Pro.local>
2018-02-09 16:13:08 -0500

    Part 1/2 done

--- Project2/digits.py
+++ Project2/digits.py
@@ -5,75 +5,45 @@ import matplotlib.cbook as cbook
 import time
 from scipy.misc import imread
 from scipy.misc import imresize
+from scipy.misc import imsave
 import matplotlib.image as mpimg
 from scipy.ndimage import filters
 import urllib
 from numpy import random
 
-import cPickle
-
 import os
 from scipy.io import loadmat
 
-# Load the MNIST digit data
+#Load the MNIST digit data
 M = loadmat("mnist_all.mat")
 
-# Display the 150-th "5" digit from the training set
-imshow(M["train5"][150].reshape((28, 28)), cmap=cm.gray)
-show()
+'''PART 1'''
 
+#save 10 images, 1 from each digit
+# for i in range(10):
+#     imsave("part1/image" + str(i) + ".png", M["train"+str(i)][10].reshape((28,28)))
+
+'''PART 2'''
+
+def compute_2(x, W, b)
+    return softmax(tanh_layer(x, W, b))
 
 def softmax(y):
     '''Return the output of the softmax function for the matrix of output y. y
     is an NxM matrix where N is the number of outputs for a single case, and M
     is the number of cases'''
-    return exp(y) / tile(sum(exp(y), 0), (len(y), 1))
-
-
-def tanh_layer(y, W, b):
+    return exp(y)/tile(sum(exp(y),0), (len(y),1))
+    
+def tanh_layer(y, W, b):    
     '''Return the output of a tanh layer for the input matrix y. y
     is an NxM matrix where N is the number of inputs for a single case, and M
     is the number of cases'''
-    return tanh(dot(W.T, y) + b)
+    return tanh(dot(W.T, y)+b)
 
+'''PART 3'''
+#looking to write the cost function
 
-def forward(x, W0, b0, W1, b1):
-    L0 = tanh_layer(x, W0, b0)
-    L1 = dot(W1.T, L0) + b1
-    output = softmax(L1)
-    return L0, L1, output
+def f(x, W, b, y):
+    res = compute_2(x,W,b)
+    return -1 * (dot(y.T, np.log(res)))
 
-
-def NLL(y, y_):
-    return -sum(y_ * log(y))
-
-
-def deriv_multilayer(W0, b0, W1, b1, x, L0, L1, y, y_):
-    '''Incomplete function for computing the gradient of the cross-entropy
-    cost function w.r.t the parameters of a neural network'''
-    dCdL1 = y - y_
-    dCdW1 = dot(L0, dCdL1.T)
-
-
-# Load sample weights for the multilayer neural network
-snapshot = cPickle.load(open("snapshot50.pkl"))
-W0 = snapshot["W0"]
-b0 = snapshot["b0"].reshape((300, 1))
-W1 = snapshot["W1"]
-b1 = snapshot["b1"].reshape((10, 1))
-
-# Load one example from the training set, and run it through the
-# neural network
-x = M["train5"][148:149].T
-L0, L1, output = forward(x, W0, b0, W1, b1)
-# get the index at which the output is the largest
-y = argmax(output)
-
-################################################################################
-# Code for displaying a feature from the weight matrix mW
-# fig = figure(1)
-# ax = fig.gca()
-# heatmap = ax.imshow(mW[:,50].reshape((28,28)), cmap = cm.coolwarm)
-# fig.colorbar(heatmap, shrink = 0.5, aspect=5)
-# show()
-################################################################################
