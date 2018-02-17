Fixed softmax numerical instability overflow

Project2/part2.py
@@ -11,7 +11,9 @@ def softmax(y):
     '''Return the output of the softmax function for the matrix of output y. y
     is an (k * m) matrix where k is the number of outputs for a single case, and m
     is the number of cases'''
-    return exp(y) / tile(sum(exp(y), 0), (len(y), 1))
+    e_y = np.exp(y - np.max(y))
+    return e_y / e_y.sum()
+    # return exp(y) / tile(sum(exp(y), 0), (len(y), 1))
 
 
 def tanh_layer(x, W, b):

Project2/part4.py
@@ -84,7 +84,7 @@ def part4(alpha, _max_iter, printing):
     #for i in range(0, M_TRAIN, 100):
     W = np.zeros((784, 10))
     # train_set, sol_set = generate_sets(M, 10)
-    train_set, sol_set = alt_gen_set(M, 5)
+    train_set, sol_set = alt_gen_set(M, 1)
     W = grad_descent(part3.f, part3.df, train_set, sol_set, W, alpha, _max_iter, 0, printing)
     print(W)
     #results += [test(M, 20, W, np.zeros((10)))]