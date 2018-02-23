a34ee6ed09c360cc1e007aa64a1b6dd28b7ef217
Alex <alexander55655@hotmail.com>
2018-02-23 04:51:21 -0500

    part 6 fixed

--- Project2/part6.py
+++ Project2/part6.py
@@ -8,6 +8,18 @@ import numpy as np
 from constants import *
 import cPickle as pickle
 
+def batch(x, y, batch_size, seed = 1):
+    n = x.shape[1]
+
+    np.random.seed(seed)
+    indx = np.random.permutation(x.shape[1])
+    
+    x_batched = x[:, indx]
+    y_batched = y[:, indx]
+
+    for m in xrange(n):
+        yield x_batched[:, m*batch_size:(m+1)*batch_size], y_batched[:,m*batch_size:(m+1)*batch_size]
+
 def part6a():
     W = pickle.load(open("part4W.p", 'rb')) #load weights
     W_copy = W.copy()
@@ -16,17 +28,16 @@ def part6a():
     #10 * 28 + 13 = 293
     #16 * 28 + 13 = 461 as our values of n, and any output may be chosen [0,9]
 
-    print W[w1, w1k]
-    print W[w2, w2k]
+    print np.mean(W)
 
-    dimension = 10
+    dimension = 70
 
     M = loadmat("mnist_all.mat")
     x, y = alt_gen_set(M, 1)
     b = np.ones(shape = (K_NUM,1))
 
-    X1 = np.linspace(0, 4, dimension)
-    X2 = np.linspace(0, 4, dimension)
+    X1 = np.linspace(0, 6, dimension)
+    X2 = np.linspace(0, 6, dimension)
 
     res_a = np.zeros(shape = (dimension,dimension))
     max_deviation = 1
@@ -37,7 +48,8 @@ def part6a():
             W[w1, w1k] = X1[i]
             W[w2, w2k] = X2[j]
             res_a[i, j] = part3.f(x,W,b,y)
-            print "done " + str(i) + " " + str(j) + " cost: " + str(res_a[i,j]) + " with w values: " + str(W[w1, w1k]) + " " + str(W[w2,w2k])
+            if j == 0:
+                print "done " + str(i) + " " + str(j) + " cost: " + str(res_a[i,j]) + " with w values: " + str(W[w1, w1k]) + " " + str(W[w2,w2k])
             
     return X1, X2, res_a
     # return W_copy[w1,w1k] + max_deviation*X1, W_copy[w2,w2k] + max_deviation*X2, res_a
@@ -53,12 +65,13 @@ def part6bcd():
     W = pickle.load(open("part5W.p", 'rb')) #load weights
     x,y = alt_gen_set(M, 1)
 
-    W[w1, w1k], W[w2, w2k] = 0.1,1.5 
-    print argmax(W)
-    W, res_b = grad_descent_2element(part3.f, part3.df, x, y, W, 0.005,20, 0, True)
 
-    W[w1, w1k], W[w2, w2k] = 0.1,1.5
-    W, res_c = grad_descent_2element(part3.f, part3.df, x, y, W, 0.005,20, 0.7, True)
+
+    W[w1, w1k], W[w2, w2k] = 3,5.5 
+    W, res_b = grad_descent_2element(part3.f, part3.df, x, y, W, 0.15,20, 0, True)
+
+    W[w1, w1k], W[w2, w2k] = 3,5.5
+    W, res_c = grad_descent_2element(part3.f, part3.df, x, y, W, 0.15,20, 0.5, True)
 
     return res_b, res_c
 
@@ -66,6 +79,8 @@ def part6bcd():
 
 def grad_descent_2element(f, df, x, y, init_W, alpha, _max_iter, momentum=0, printing=True):
     print("------------------------- Starting Grad Descent -------------------------")
+    gen = batch(x, y, 5000, 1)
+    
     EPS = 1e-5  # EPS = 10**(-5)
     prev_t = init_W - 10 * EPS
     prev_grad = 0
@@ -76,10 +91,11 @@ def grad_descent_2element(f, df, x, y, init_W, alpha, _max_iter, momentum=0, pri
 
     res = []
     while iter < max_iter: #and norm(t - prev_t) > EPS:
+        xgen,ygen= next(gen)
         prev_t = W.copy()
         res.append((W[w1, w1k], W[w2, w2k]))
         b = np.zeros(shape=(K_NUM,1))
-        grad = df(x, y, W, b)
+        grad = df(xgen, ygen, W, b)
         V = momentum * V + alpha * grad
         # W -= alpha * grad
         W[w1, w1k] -= V[w1, w1k]
