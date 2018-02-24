977c181e21547526d0a45e7cc92240246fcc5a94
Shichen Lu <shichenl2@gmail.com>
2018-02-24 17:27:53 -0600

    Temp code for p4

--- Project2/part4.py
+++ Project2/part4.py
@@ -35,14 +35,24 @@ def grad_descent(f, df, x, y, init_W, alpha_w, alpha_b, _max_iter, printing=True
 
 def generate_sets(database, size):
     train_set = np.zeros((size, N_NUM))
-    sol_set = np.zeros((size, 10))
+    val_set = np.zeros((6000, N_NUM))
+    train_set_sol = np.zeros((size, K_NUM))
+    val_set_sol = np.zeros((6000, K_NUM))
+    count = np.zeros((10,1))
+
+    for i in range(6000):
+        rand_dgt = np.random.random_integers(0,9)
+        val_set[i] = database["train"+str(rand_dgt)][int(count[rand_dgt][0])] / 255.0
+        val_set_sol[i][rand_dgt] = 1
+        count[rand_dgt] += 1
 
     for i in range(size):
         rand_dgt = np.random.random_integers(0,9)
-        train_set[i] = database["train"+str(rand_dgt)][i] / 255.0
-        sol_set [i][rand_dgt] = 1
+        train_set[i] = database["train"+str(rand_dgt)][int(count[rand_dgt][0])] / 255.0
+        train_set_sol[i][rand_dgt] = 1
+        count[rand_dgt] += 1
 
-    return train_set.T, sol_set.T
+    return train_set.T, train_set_sol.T, val_set.T, val_set_sol.T
 
 def alt_gen_set(database, scale):
     x = np.zeros(shape = (N_NUM, M_TRAIN))
@@ -74,25 +84,45 @@ def test(database, size, W, b):
 
     return correct/float(size)
 
+def test2(database, solutions, W, b):
+    correct = 0
+    database = database.T
+    solutions = solutions.T
+
+    for i in range(len(database)):
+        guess = part2.forward(database[i], W, b)
+        if np.argmax(guess) == np.argmax(solutions[i]):
+            correct += 1
+
+    return correct/float(len(database))
+
 
 def part4(alpha_w, alpha_b, _max_iter, printing):
     M = loadmat("mnist_all.mat")
 
-    results = []
+    results_val = []
+    results_train = []
     x = []
 
-    #TODO: bias
-    #for i in range(0, M_TRAIN, 100):
-    W = np.zeros((784, 10))
-    # train_set, sol_set = generate_sets(M, 1000)
-    train_set, sol_set = alt_gen_set(M, 1)
-    W, b = grad_descent(part3.f, part3.df, train_set, sol_set, W, alpha_w, alpha_b, _max_iter, printing)
-    pickle.dump( W, open( "part4W.p", "wb" ) )
-    #b = np.zeros((10, 1))
-    print(W, b)
-    print("Testing... {}% correct".format(test(M, 500, W, b)*100))
-    #results += [test(M, 20, W, np.zeros((10)))]
-    #x += [0]
 
+    for i in range(5000, M_TRAIN - 7000, 5000):
+        if i<5000:
+            continue
+        print(i)
+        W = np.zeros((784, 10))
+        W = np.random.rand(784, 10)
+        train_set, train_set_sol, val_set, val_set_sol = generate_sets(M, i)
+        # train_set, sol_set = alt_gen_set(M, 1)
+        W, b = grad_descent(part3.f, part3.df, train_set, train_set_sol, W, alpha_w, alpha_b, _max_iter, printing)
+        #b = np.ones((10, 1))*100
+        #print(W, b)
+        # print("Testing... {}% correct".format(test(M, 500, W, b)*100))
+        results_val += [test2(val_set, val_set_sol, W, b)]
+        results_train += [test2(train_set, train_set_sol, W, b)]
+        x += [i]
 
-    #plt.plot(results, x)
+    pickle.dump(W, open("part4W.p", "wb"))
+    plt.plot(x, results_val)
+    plt.plot(x, results_train)
+    plt.legend(["Validation Set Accuracy", "Training Set Accuracy"])
+    plt.show()
