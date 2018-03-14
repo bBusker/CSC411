13a85a1cbd18f789cb9352ae4cdddda939d1267a
Shichen Lu <shichenl2@gmail.com>
2018-03-13 23:35:03 -0400

    Cleaned up implementation of naive bayes

--- Project3/fake.py
+++ Project3/fake.py
@@ -1,81 +1,37 @@
 import random
+import nb_trainer
 from math import *
 
-class sets:
-    training = 0
-    validation = 1
-    test = 2
-
-f_fake = open("clean_fake.txt")
+# Open and read headlines from file
 f_real = open("clean_real.txt")
+f_fake = open("clean_fake.txt")
 
-fakes = [str.split(line) for line in f_fake]
 reals = [str.split(line) for line in f_real]
+fakes = [str.split(line) for line in f_fake]
 
-random.shuffle(fakes)
+
+# Randomize and generate training, validation, test sets
+# random.seed(0)
 random.shuffle(reals)
+random.shuffle(fakes)
 
-sets_fakes = {sets.training: fakes[:909], sets.validation: fakes[909:1104], sets.test: fakes[1104:1298]}
-sets_reals = {sets.training: reals[:1378], sets.validation: reals[1378:1673], sets.test: reals[1673:1968]}
-
-priors_fakes = {"total": 0}
-for headline in sets_fakes[sets.validation]:
-    for word in headline:
-        if word in priors_fakes:
-            priors_fakes[word] += 1
-        else:
-            priors_fakes[word] = 1
-        priors_fakes["total"] += 1
-
-priors_reals = {"total": 0}
-for headline in sets_reals[sets.validation]:
-    for word in headline:
-        if word in priors_reals:
-            priors_reals[word] += 1
-        else:
-            priors_reals[word] = 1
-        priors_reals["total"] += 1
+sets_reals = {"train": reals[:1378], "val": reals[1378:1673], "test": reals[1673:1968]}
+sets_fakes = {"train": fakes[:909], "val": fakes[909:1104], "test": fakes[1104:1298]}
 
 
-p_fake = len(fakes)/float(len(reals)+len(fakes))
+# Generate Priors for naive bayes on validation set
+priors_reals, priors_fakes = nb_trainer.generate_priors(sets_reals, sets_fakes, "train")
 p_real = len(reals)/float(len(fakes)+len(reals))
-
-def nb_predictor(headline, priors_reals, priors_fakes, p_fake): #TODO: train on training set, m and p paratemetrs???
-    #Returns probability of the headline being fake news
-    #p(y|x) = p(x|y)*p(y)/p(x)
-
-    p_x_noty = 0
-    p_x_y = 0
-    p_y = p_fake
-
-    reals_total = priors_reals["total"] #TODO: totals counting
-    fakes_total = priors_fakes["total"]
+p_fake = len(fakes)/float(len(reals)+len(fakes))
 
 
-    for word in headline:
-        try:
-            c_real = priors_reals[word]
-        except:
-            c_real = 1
-            reals_total += 1
-
-        try:
-            c_fake = priors_fakes[word]
-        except:
-            c_fake = 1
-            fakes_total += 1
-
-        p_x_noty += log(c_real) - log(reals_total)
-        p_x_y += log(c_fake) - log(fakes_total)
-
-    p_yx = exp(p_x_y) * p_y / (exp(p_x_y) * p_y + exp(p_x_noty) * (1-p_y))
-
-    return p_yx
-
-
+# Testing
 correct = 0
 print("testing on fake training set")
-for i in range(len(sets_fakes[sets.test])):
-    if nb_predictor(sets_fakes[sets.test][i], priors_reals, priors_fakes, p_fake) >= 0.5:
+for i in range(len(sets_fakes["test"])):
+    prediction = nb_trainer.nb_predictor(sets_fakes["test"][i], priors_reals, priors_fakes, p_fake)
+    if prediction >= 0.5:
         correct += 1
-print("correct: {}".format(float(correct)/len(sets_fakes[sets.test])))
+    if prediction >= 1:
+        print("INVALID PREDICTION: {}".format(prediction))
+print("correct: {}".format(float(correct)/len(sets_fakes["test"])))
---
+++ Project3/nb_trainer.py
@@ -0,0 +1,54 @@
+from math import *
+
+def generate_priors(sets_reals, sets_fakes, set_choice):
+    priors_reals = {"count_total": 0}
+    for headline in sets_reals[set_choice]:
+        for word in headline:
+            if word in priors_reals:
+                priors_reals[word] += 1
+            else:
+                priors_reals[word] = 1
+            priors_reals["count_total"] += 1
+
+    priors_fakes = {"count_total": 0}
+    for headline in sets_fakes[set_choice]:
+        for word in headline:
+            if word in priors_fakes:
+                priors_fakes[word] += 1
+            else:
+                priors_fakes[word] = 1
+            priors_fakes["count_total"] += 1
+
+    return priors_reals, priors_fakes
+
+
+def nb_predictor(headline, priors_reals, priors_fakes, p_fake):
+    #Returns probability of the headline being fake news
+    #p(y|x) = p(x|y)*p(y)/(p(x|y)*p(y) + p(x|~y)*p(~y))
+
+    m = 2 # Virtual Examples
+    p_hat = 0.5 # Virtual Example Prior
+
+    p_x_given_real = 0
+    p_x_given_fake = 0
+
+    reals_total = priors_reals["count_total"] #TODO: totals counting
+    fakes_total = priors_fakes["count_total"]
+
+    for word in headline:
+        try:
+            c_real = priors_reals[word]
+        except:
+            c_real = 0
+
+        try:
+            c_fake = priors_fakes[word]
+        except:
+            c_fake = 0
+
+        p_x_given_real += log(c_real + m*p_hat) - log(reals_total + m)
+        p_x_given_fake += log(c_fake + m*p_hat) - log(fakes_total + m)
+
+    p_yx = exp(p_x_given_fake) * p_fake / (exp(p_x_given_fake) * p_fake + exp(p_x_given_real) * (1-p_fake))
+
+    return p_yx
